549f542dd536ced9c221373ac2ec928d19c1b891
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2023-01-06 12:54:42 -0600

    fetching data from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,41 @@
 import { createClient } from 'contentful' 
+import RecipeCard from '../components/RecipeCard'
 
 export async function getStaticProps(){
     const client = createClient({
-        space: process.env.SPACE_ID,
-        accessToken: process.env.ACCESS_TOKEN,
+        space: process.env.NEXT_PUBLIC_SPACE_ID,
+        accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
     })
 
-    const res = await client.getEntries({ content_type: 'recipe'})
+    const res = await client.getEntries({content_type: 'recipe'})
+    return {
+        props: {
+            recipes: res.items
+        }
+    }
 }
 
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+export default function Recipes({recipes}) {
+    // console.log(recipes);
+
+    return (
+        <div className="recipe-list">
+            {recipes.map(recipe => (
+                <RecipeCard key={recipe.sys.id} recipe={recipe}/>
+                    
+                
+            ))}
+
+        <style jsx>
+            {`
+                .recipe-list {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                grid-gap: 20px 60px;
+                }
+            `}
+        </style>
+
+        </div>
+    )
 }
