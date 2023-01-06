40e8cbb41ce970294959970d343965ee1bae6da5
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2023-01-06 13:19:27 -0600

    recipe skeleton & incremental static regeneration

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -31,67 +32,71 @@ export const getStaticProps = async ({ params }) => {
   })
 
   return {
-    props: { recipe: items[0] }
+    props: { recipe: items[0] },
+    revalidate: 10
   }
 
 }
 
 export default function RecipeDetails({ recipe }) {
+
+    if(!recipe) return <div><Skeleton /></div>
+
     const { featuredImage, title, cookingTime, ingredients, method} = recipe.fields
-  console.log(recipe)
-  
-  return (
-    <div className='banner'>
-        <Image 
-            src={`https:${featuredImage.fields.file.url}`} 
-            width={featuredImage.fields.file.details.image.width}
-            height={featuredImage.fields.file.details.image.height}
-        />
+    // console.log(recipe)
+    
+    return (
+        <div className='banner'>
+            <Image 
+                src={`https:${featuredImage.fields.file.url}`} 
+                width={featuredImage.fields.file.details.image.width}
+                height={featuredImage.fields.file.details.image.height}
+            />
 
-        <h2>{title}</h2>
+            <h2>{title}</h2>
 
-        <div className='info'>
-            <p>Takes about {cookingTime} mins to cook.</p>
-            <h3>Ingredients:</h3>
+            <div className='info'>
+                <p>Takes about {cookingTime} mins to cook.</p>
+                <h3>Ingredients:</h3>
 
-            {ingredients.map(ing => {
-                return (
-                    <span key={ing}>{ ing }</span>
-                )
-            })}
+                {ingredients.map(ing => {
+                    return (
+                        <span key={ing}>{ ing }</span>
+                    )
+                })}
+            </div>
+
+            <div className="method">
+                <h3>Method:</h3>
+                <div>{documentToReactComponents(method)}</div>
+            </div>
+
+            <style jsx>{`
+                h2,h3 {
+                text-transform: uppercase;
+                }
+                .banner h2 {
+                margin: 0;
+                background: #fff;
+                display: inline-block;
+                padding: 20px;
+                position: relative;
+                top: -60px;
+                left: -10px;
+                transform: rotateZ(-1deg);
+                box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+                }
+                .info p {
+                margin: 0;
+                }
+                .info span::after {
+                content: ", ";
+                }
+                .info span:last-child::after {
+                content: ".";
+                }
+            `}</style>
+        
         </div>
-
-        <div className="method">
-            <h3>Method:</h3>
-            <div>{documentToReactComponents(method)}</div>
-        </div>
-
-        <style jsx>{`
-            h2,h3 {
-            text-transform: uppercase;
-            }
-            .banner h2 {
-            margin: 0;
-            background: #fff;
-            display: inline-block;
-            padding: 20px;
-            position: relative;
-            top: -60px;
-            left: -10px;
-            transform: rotateZ(-1deg);
-            box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-            }
-            .info p {
-            margin: 0;
-            }
-            .info span::after {
-            content: ", ";
-            }
-            .info span:last-child::after {
-            content: ".";
-            }
-        `}</style>
-      
-    </div>
-  )
+    )
 }
